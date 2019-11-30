b70a83bb921533e7fd58212239328391e59e0aa8
Aleksandra Marevskaia <am02316@surrey.ac.uk>
2019-11-30 13:27:34 +0000

    Added contact form

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.5.1'
 gem 'haml-rails'
 gem 'bootstrap-sass','~>3.3.6'
 gem 'modernizr-rails'
+gem 'rails-controller-testing'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,4 +10,30 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'p', 'Share real stories from hisory with others!'
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'MerNeith'
+    assert_select 'h1',  'MerNeith'
+    assert_select 'p', 'If you have any enquiries, please fill the form below.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+    end
+
+    test "should post request contact" do
+      post request_contact_url, params:
+        {name:"Alex",email:"alex123@surrey.ac.uk", telephone: "1234567890", message: "Help me"}
+      assert_response :redirect
+        assert_nil flash[:alert]
+        assert_not_empty flash[:notice]
+      end
 end
